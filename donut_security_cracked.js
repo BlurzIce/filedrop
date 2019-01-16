27fbd412f70f0220fa4dc8738a4d590a2af3594c
BlurzIce <37122802+BlurzIce@users.noreply.github.com>
2019-01-16 14:04:28 +0000

    Update donut_security_cracked.js

--- donut_security_cracked.js
+++ donut_security_cracked.js
@@ -1,10 +1,6 @@
 function validate(username, password, dUser, dPass) {
-  // Dev note: enhanced security measures!
-  // The users password in our database is always prefixed with "pass_" for security
-  if (username === dUser && password === "pass_" + dPass) {
-    handleLogin(username, password);
-  } else {
-    handleLogin(username, password);
-  }
+  handleLogin(username, password);
+
+  
 }
 
